5fa35b868a7c727d0d2a04ce0cdf23d0b25cd589
“garrettruble1” <garrettruble1@gmail.com>
2020-05-27 19:15:36 -0400

    Classes & State Activity

--- src/App.js
+++ src/App.js
@@ -53,41 +53,4 @@ class App extends React.Component {
   }
 }
 
-/*function App() {
-  let dogs = [
-    {
-      name: "Scruffles",
-      type: "Chihuahua",
-    },
-    {
-      name: "Snarf",
-      type: "Border Collie",
-    },
-    {
-      name: "Wookie",
-      type: "English Sheepdog",
-    },
-  ];
-
-  function pickNewFavorite() {
-    let newDogIndex = Math.floor(Math.random() * dogs.length);
-    return dogs[newDogIndex];
-  }
-
-  let favoriteDog = pickNewFavorite();
-
-  return (
-    <div className="dogs">
-      <Header numDogs={dogs.length} />
-      <h2>My Dogs</h2>
-      <div>
-        {dogs.map((dog) => (
-          <Dog dog={dog} />
-        ))}
-      </div>
-      <h2>My Favorite dog is {favoriteDog.name}</h2>
-    </div>
-  );
-}*/
-
 export default App;
